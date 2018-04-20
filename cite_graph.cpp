8145e6883ef6597d1de9c20450f01e8965c77caa
Arunavha Chanda <ac3806@columbia.edu>
2018-04-20 03:30:51 -0400

    Working large top-sort test

--- cite_graph.cpp
+++ cite_graph.cpp
@@ -9,9 +9,13 @@
 #include <fstream>
 #include <sstream>
 #include <set>
+#include <map>
+#include <chrono>
 #include "boost/graph/topological_sort.hpp"
 #include "boost/graph/adjacency_list.hpp"
 #include "boost/graph/graph_traits.hpp"
+#include "boost/exception/diagnostic_information.hpp"
+#include "boost/exception_ptr.hpp"
 #include "boost/config.hpp"
 
 using namespace NMSPC;
@@ -23,7 +27,8 @@ int main(){
     graph<string> cite_graph; // an empty graph.
 
     ifstream cites;
-    cites.open("./Cit-HepTh.txt");
+    cites.open("./cit-Patents.txt");
+    //cites.open("./Cit-HepTh.txt");
     string line;
     int skip = 0;
     int vert_tot = 0;
@@ -38,10 +43,10 @@ int main(){
 
             istringstream linestream(line);
             int node_old, node_new;
-            cout <<line<<endl;
+            //cout <<line<<endl;
 
             linestream >> node_new >> node_old;
-            cout << "node_new: " << node_new << " and node_old: " << node_old <<endl;
+            //cout << "node_new: " << node_new << " and node_old: " << node_old <<endl;
             const string new_label_const = to_string(node_new);
             const string old_label_const = to_string(node_old);
 
@@ -62,18 +67,27 @@ int main(){
             }
             auto index_old = cite_graph.get_index(old_label);
             auto index_new = cite_graph.get_index(new_label);
-            cout << "old index: " << *index_old << " and new index: " << *index_new <<endl;
+            //cout << "old index: " << *index_old << " and new index: " << *index_new <<endl;
             cite_graph.add_edge(*index_old, *index_new);
         }
     }
 
-    if(auto topological_order = get_topological_order(cite_graph)){
+    auto start = std::chrono::system_clock::now();
+    auto topological_order = get_topological_order(cite_graph);
+    auto end = std::chrono::system_clock::now(); 
+    std::chrono::duration<double> elapsed_seconds = end-start;
+
+    if(topological_order){
         cout << "topological order exists" << '\n';
-        for(auto &s : *topological_order) cout << s << '\n';
+        /*
+        for(auto &s : *topological_order) cout << cite_graph[s].val << '\n';
         cout << '\n';
+        */
     }else{
         cout << "topological order does not exist" << '\n';
     }
+
+    std::cout << "top-sort time for graph_explorer is: " << elapsed_seconds.count() << "s\n";
 
     cites.close();
 
@@ -81,15 +95,16 @@ int main(){
 
     using namespace boost;
 
-    typedef adjacency_list<vecS, vecS, directedS, 
-    property<vertex_color_t, default_color_type> > Graph;
-
+    typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::directedS> Graph;
     typedef boost::graph_traits<Graph>::vertex_descriptor Vertex;
 
-    Pair edges[360000];
+    Graph g(vert_tot);
+
+    //Pair edges[1100000];
 
     ifstream cites_boost;
-    cites_boost.open("./Cit-HepTh.txt");
+    cites_boost.open("./cit-Patents.txt");
+    //cites_boost.open("./Cit-HepTh.txt");
     skip = 0;
     int edge_no = 0;
 
@@ -102,28 +117,56 @@ int main(){
 
             istringstream linestream(line);
             int node_old, node_new;
-            cout <<line<<endl;
+            //cout <<line<<endl;
 
             linestream >> node_new >> node_old;
-            cout << "node_new: " << node_new << " and node_old: " << node_old <<endl;
-            edges[edge_no] = Pair(node_old,node_new);
+            //cout << "node_new: " << node_new << " and node_old: " << node_old <<endl;
+            //edges[edge_no] = Pair(node_old,node_new);
+
+            const string new_label_const = to_string(node_new);
+            const string old_label_const = to_string(node_old);
+
+            string new_label = new_label_const;
+            string old_label = old_label_const;
+
+            auto index_old = cite_graph.get_index(old_label);
+            auto index_new = cite_graph.get_index(new_label);
+
+            boost::add_edge(*index_old, *index_new, g);
             ++edge_no;
         }
     }
     
     cites_boost.close();
 
-    Graph G(edges, edges + edge_no, edge_no);
+    //Graph G(edges, edges + edge_no, edge_no);
     
-    boost::property_map<Graph, vertex_index_t>::type id = get(vertex_index, G);
+    boost::property_map<Graph, vertex_index_t>::type id = get(vertex_index, g);
     typedef std::vector< Vertex > container;
     container c;
-    topological_sort(G, std::back_inserter(c));
-    std::cout << "A topological ordering: ";
-    for (container::reverse_iterator ii = c.rbegin(); 
-       ii != c.rend(); ++ii)
-    std::cout << id[*ii] << " ";
-    std::cout << std::endl;
+    auto start_boost = std::chrono::system_clock::now();
+    try{
+        topological_sort(g, std::back_inserter(c));
+        auto end_boost = std::chrono::system_clock::now(); 
+        std::chrono::duration<double> elapsed_seconds_boost = end_boost-start_boost;
+
+        cout << "A topological ordering exists\n";
+        cout << "top-sort time for boost is: " << elapsed_seconds_boost.count() << "s\n";
+
+        /*
+        for ( container::reverse_iterator ii=c.rbegin(); ii!=c.rend(); ++ii)
+            cout << cite_graph[id[*ii]].val << "\n";
+        cout << endl;
+        */
+    }
+    catch (boost::exception const& e) {
+        auto end_boost = std::chrono::system_clock::now(); 
+        std::chrono::duration<double> elapsed_seconds_boost = end_boost-start_boost;
+        cout << "top-sort time for boost is: " << elapsed_seconds_boost.count() << "s\n";
+        std::string diag = boost::diagnostic_information(e);
+        printf("Boost exception: %s", diag);
+    }
+
 
     return 0;
 }
