15c19b00bcc2322f70f7067e4350627ee5ea487c
amitgbhat <amitbhat@Amits-MacBook-Pro-2.local>
2018-04-24 17:13:15 -0400

    Fixing build issues in cite_graph.cpp

--- cite_graph.cpp
+++ cite_graph.cpp
@@ -37,7 +37,7 @@ void dfs(int u){
 int main(int argc, char** argv){
     string FILENAME = (argc > 1)? argv[1]:"./cit-Patents.txt";
     // another type of graph
-    graph<string> cite_graph; // an empty graph.
+    graph<string, int> cite_graph; // an empty graph.
 
     ifstream cites;
     cites.open(FILENAME);
@@ -84,7 +84,7 @@ int main(int argc, char** argv){
             auto index_old = cite_graph.get_index(old_label);
             auto index_new = cite_graph.get_index(new_label);
             //cout << "old index: " << *index_old << " and new index: " << *index_new <<endl;
-            cite_graph.add_edge(*index_old, *index_new);
+            cite_graph.add_edge(*index_old, *index_new, 1);
             adj[*index_old].push_back(*index_new);
             ++edge_tot;
         }
